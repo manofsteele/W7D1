a9ed280da3d0054c35aeb16683c461351e580d7b
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-31 16:29:35 -0700

    Start rails project

---
+++ todos/backend/todos/app/controllers/todos_controller.rb
@@ -0,0 +1,38 @@
+class TodosController < ApplicationController
+  def show
+    render json: Todo.find(params[:id])
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    render json: Todo.all
+  end
+
+  def update
+    @todo = Todo.update(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    render json: @todo
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :body, :done)
+  end
+
+end
